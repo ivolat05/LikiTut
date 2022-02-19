30db6a27174065e004d158d173c1999597786a31
andrei_labadaev <andrei-labadaev@mail.ru>
2022-02-19 10:48:27 +0300

    uppdate 10.48

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -60,104 +60,105 @@ $(function () {
         // класс формы поиска id
         let inp = document.getElementById(idFormSearch)
         let currentFocus;
-        /* выполнение функции, когда кто-то пишет в текстовом поле: */
-        inp.addEventListener("input", function (e) {
-            let a, b, i, val = this.value;
-            /* закрыть все уже открытые списки значений автозаполнения */
-            closeAllLists();
-            if (!val) { return false; }
-            currentFocus = -1;
-            /* создайте элемент DIV, который будет содержать элементы (значения): */
-            a = document.createElement("DIV");
-            a.setAttribute("id", this.id + "autocomplete-list");
-            a.setAttribute("class", "autocomplete-items");
-            /* добавьте элемент DIV в качестве дочернего элемента контейнера автозаполнения: */
-            this.parentNode.appendChild(a);
-            /* для каждого элемента в массиве... */
-            for (i = 0; i < sortList.length; i++) {
-                /* проверьте, начинается ли элемент с тех же букв, что и значение текстового поля: */
-                if (sortList[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                    /* создайте элемент DIV для каждого соответствующего элемента: */
-                    b = document.createElement("DIV");
-                    b.classList.add('autocomplete-items-row')
-                    /* сделайте соответствующие буквы жирным шрифтом: */
-                    b.innerHTML = "<span class='autocompliter-literal'>" + sortList[i].substr(0, val.length) + "</span>";
-                    b.innerHTML += sortList[i].substr(val.length);
-                    /* вставьте поле ввода, которое будет содержать значение текущего элемента массива: */
-                    b.innerHTML += "<input type='hidden' value='" + sortList[i] + "'>";
-                    /* выполнение функции, когда кто-то нажимает на значение элемента (элемент DIV): */
-                    b.addEventListener("click", function (e) {
-                        /* вставьте значение для текстового поля автозаполнения: */
-                        inp.value = this.getElementsByTagName("input")[0].value;
-                        /* закройте список значений автозаполнения,
-                        (или любые другие открытые списки значений автозаполнения : */
-                        closeAllLists();
-                    });
-                    a.appendChild(b);
+        if (inp) {
+            /* выполнение функции, когда кто-то пишет в текстовом поле: */
+            inp.addEventListener("input", function (e) {
+                let a, b, i, val = this.value;
+                /* закрыть все уже открытые списки значений автозаполнения */
+                closeAllLists();
+                if (!val) { return false; }
+                currentFocus = -1;
+                /* создайте элемент DIV, который будет содержать элементы (значения): */
+                a = document.createElement("DIV");
+                a.setAttribute("id", this.id + "autocomplete-list");
+                a.setAttribute("class", "autocomplete-items");
+                /* добавьте элемент DIV в качестве дочернего элемента контейнера автозаполнения: */
+                this.parentNode.appendChild(a);
+                /* для каждого элемента в массиве... */
+                for (i = 0; i < sortList.length; i++) {
+                    /* проверьте, начинается ли элемент с тех же букв, что и значение текстового поля: */
+                    if (sortList[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                        /* создайте элемент DIV для каждого соответствующего элемента: */
+                        b = document.createElement("DIV");
+                        b.classList.add('autocomplete-items-row')
+                        /* сделайте соответствующие буквы жирным шрифтом: */
+                        b.innerHTML = "<span class='autocompliter-literal'>" + sortList[i].substr(0, val.length) + "</span>";
+                        b.innerHTML += sortList[i].substr(val.length);
+                        /* вставьте поле ввода, которое будет содержать значение текущего элемента массива: */
+                        b.innerHTML += "<input type='hidden' value='" + sortList[i] + "'>";
+                        /* выполнение функции, когда кто-то нажимает на значение элемента (элемент DIV): */
+                        b.addEventListener("click", function (e) {
+                            /* вставьте значение для текстового поля автозаполнения: */
+                            inp.value = this.getElementsByTagName("input")[0].value;
+                            /* закройте список значений автозаполнения,
+                            (или любые другие открытые списки значений автозаполнения : */
+                            closeAllLists();
+                        });
+                        a.appendChild(b);
+                    }
                 }
-            }
 
-            // стилизация скрола
-            $(".autocomplete-items").mCustomScrollbar({
+                // стилизация скрола
+                $(".autocomplete-items").mCustomScrollbar({
 
+                });
             });
-        });
-        /* выполнение функции нажимает клавишу на клавиатуре: */
-        inp.addEventListener("keydown", function (e) {
-            let x = document.getElementById(this.id + "autocomplete-list");
-            if (x) x = x.getElementsByTagName("div");
-            if (e.keyCode == 40) {
-                /* Если нажата клавиша со стрелкой вниз,
-                увеличение текущей переменной фокуса: */
-                currentFocus++;
-                /* и сделать текущий элемент более видимым: */
-                addActive(x);
-            } else if (e.keyCode == 38) { //вверх
-                /* Если нажата клавиша со стрелкой вверх,
-                уменьшите текущую переменную фокуса: */
-                currentFocus--;
-                /* и сделать текущий элемент более видимым: */
-                addActive(x);
-            } else if (e.keyCode == 13) {
-                /* Если нажата клавиша ENTER, предотвратите отправку формы, */
-                e.preventDefault();
-                if (currentFocus > -1) {
-                    /* и имитировать щелчок по элементу "active": */
-                    if (x) x[currentFocus].click();
+            /* выполнение функции нажимает клавишу на клавиатуре: */
+            inp.addEventListener("keydown", function (e) {
+                let x = document.getElementById(this.id + "autocomplete-list");
+                if (x) x = x.getElementsByTagName("div");
+                if (e.keyCode == 40) {
+                    /* Если нажата клавиша со стрелкой вниз,
+                    увеличение текущей переменной фокуса: */
+                    currentFocus++;
+                    /* и сделать текущий элемент более видимым: */
+                    addActive(x);
+                } else if (e.keyCode == 38) { //вверх
+                    /* Если нажата клавиша со стрелкой вверх,
+                    уменьшите текущую переменную фокуса: */
+                    currentFocus--;
+                    /* и сделать текущий элемент более видимым: */
+                    addActive(x);
+                } else if (e.keyCode == 13) {
+                    /* Если нажата клавиша ENTER, предотвратите отправку формы, */
+                    e.preventDefault();
+                    if (currentFocus > -1) {
+                        /* и имитировать щелчок по элементу "active": */
+                        if (x) x[currentFocus].click();
+                    }
+                }
+            });
+            function addActive(x) {
+                /* функция для классификации элемента как "active": */
+                if (!x) return false;
+                /* начните с удаления "активного" класса для всех элементов: */
+                removeActive(x);
+                if (currentFocus >= x.length) currentFocus = 0;
+                if (currentFocus < 0) currentFocus = (x.length - 1);
+                /*добавить класса "autocomplete-active": */
+                x[currentFocus].classList.add("autocomplete-active");
+            }
+            function removeActive(x) {
+                /* функция для удаления "активного" класса из всех элементов автозаполнения: */
+                for (let i = 0; i < x.length; i++) {
+                    x[i].classList.remove("autocomplete-active");
                 }
             }
-        });
-        function addActive(x) {
-            /* функция для классификации элемента как "active": */
-            if (!x) return false;
-            /* начните с удаления "активного" класса для всех элементов: */
-            removeActive(x);
-            if (currentFocus >= x.length) currentFocus = 0;
-            if (currentFocus < 0) currentFocus = (x.length - 1);
-            /*добавить класса "autocomplete-active": */
-            x[currentFocus].classList.add("autocomplete-active");
-        }
-        function removeActive(x) {
-            /* функция для удаления "активного" класса из всех элементов автозаполнения: */
-            for (let i = 0; i < x.length; i++) {
-                x[i].classList.remove("autocomplete-active");
-            }
-        }
-        function closeAllLists(elmnt) {
-            /* закройте все списки автозаполнения в документе,
-            кроме того, который был передан в качестве аргумента: */
-            let x = document.getElementsByClassName("autocomplete-items");
-            for (var i = 0; i < x.length; i++) {
-                if (elmnt != x[i] && elmnt != inp) {
-                    x[i].parentNode.removeChild(x[i]);
+            function closeAllLists(elmnt) {
+                /* закройте все списки автозаполнения в документе,
+                кроме того, который был передан в качестве аргумента: */
+                let x = document.getElementsByClassName("autocomplete-items");
+                for (var i = 0; i < x.length; i++) {
+                    if (elmnt != x[i] && elmnt != inp) {
+                        x[i].parentNode.removeChild(x[i]);
+                    }
                 }
             }
+            /* выполнение функции, когда кто-то щелкает в документе: */
+            document.addEventListener("click", function (e) {
+                closeAllLists(e.target);
+            });
         }
-        /* выполнение функции, когда кто-то щелкает в документе: */
-        document.addEventListener("click", function (e) {
-            closeAllLists(e.target);
-        });
-
     }
 
 
@@ -368,4 +369,33 @@ $(function () {
         }
     }
     basketProduct();
+
+    // select
+    let select = function () {
+        let selectHeader = document.querySelectorAll('.select__header');
+        let selectItem = document.querySelectorAll('.select__item');
+        if (selectHeader) {
+            selectHeader.forEach((item) => {
+                item.addEventListener('click', selectToggle)
+            })
+
+            selectItem.forEach((item) => {
+                item.addEventListener('click', selectChoose)
+            })
+
+            function selectToggle() {
+                this.parentElement.classList.toggle('is-active');
+            }
+
+            function selectChoose() {
+                let text = this.innerText;
+                let select = this.closest('.select');
+                let currentText = select.querySelector(".select__current");
+                currentText.innerText = text;
+                select.classList.remove('is-active');
+            }
+        }
+
+    }
+    select();
 })
